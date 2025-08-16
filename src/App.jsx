05efb8866c3edeb565f3309bba07ea89c618d4fb
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2025-08-16 15:29:47 +0100

    feat: add Lenis for smooth scrolling functionality
    Integrate Lenis library to enhance scrolling experience with smooth animations and event tracking. Added initialization and scroll event listener in App.jsx, and updated package dependencies accordingly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,90 @@
-import './App.css'
-import { Navbar, Hero, GetStarted, Features, Testimonials, DownloadAds, FAQ, CTA, Footer } from './components';
- import { hero_pattern, dowloadAds_pattern, dowloadAds_pattern_sm, noise_bg } from './assets';
- 
+import "./App.css";
+import {
+  Navbar,
+  Hero,
+  GetStarted,
+  Features,
+  Testimonials,
+  DownloadAds,
+  FAQ,
+  CTA,
+  Footer,
+} from "./components";
+import {
+  hero_pattern,
+  dowloadAds_pattern,
+  dowloadAds_pattern_sm,
+  noise_bg,
+} from "./assets";
+import Lenis from "lenis";
+
 function App() {
+  // Initialize Lenis
+
+  const lenis = new Lenis({
+    autoRaf: true,
+  });
+
+  // Listen for the scroll event and log the event data
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
   // Define the radial gradient background style
-   const radialGradientStyle = {
+  const radialGradientStyle = {
     background: `
       radial-gradient(50% 50% at 50% 50%, #385334 0%, #041402 100%), 
       url(${noise_bg})`,
-      backgroundBlendMode: 'overlay',
+    backgroundBlendMode: "overlay",
     // Other styles for your component
-
-    
   };
 
-  const HeroBgPatternSectionStyle = () =>  (
+  const HeroBgPatternSectionStyle = () => (
     <>
       <div className="hidden md:block absolute top-0 right-0 bg-cover bg-no-repeat">
         <img src={hero_pattern} alt="hero_pattern" />
       </div>
       <div className="sm:block md:hidden absolute w-[75%] top-0 right-0 bg-cover bg-no-repeat">
-          <img src={hero_pattern} alt="hero_pattern" />
+        <img src={hero_pattern} alt="hero_pattern" />
       </div>
     </>
   );
 
-  const DowloadAdsBgPatternSectionStyle = () =>  (
+  const DowloadAdsBgPatternSectionStyle = () => (
     <>
       <div className="hidden md:block absolute top-0 right-0 bg-cover bg-no-repeat h-[100%]">
-        <img src={dowloadAds_pattern} className='h-[100%]' alt="downloadads_pattern" />
+        <img
+          src={dowloadAds_pattern}
+          className="h-[100%]"
+          alt="downloadads_pattern"
+        />
       </div>
       <div className="sm:block md:hidden absolute top-0 right-0 bg-cover bg-no-repeat">
-          <img src={dowloadAds_pattern_sm} alt="downloadads_pattern" /> 
+        <img src={dowloadAds_pattern_sm} alt="downloadads_pattern" />
       </div>
     </>
   );
 
   return (
     <>
-      <div className='w-full overflow-hidden mx-auto'>
-          <div style={radialGradientStyle} className='relative'>
-            <HeroBgPatternSectionStyle />
-            <Navbar/>
-            <Hero/>
-          </div>
-          <GetStarted/>
-          <Features/>
-          <Testimonials/>
-          <div style={radialGradientStyle} className='relative'>
-            <DowloadAdsBgPatternSectionStyle/>
-            <DownloadAds/>
-          </div>
-          <FAQ/>
-          <CTA/>
-          <Footer/>
+      <div className="w-full overflow-hidden mx-auto">
+        <div style={radialGradientStyle} className="relative">
+          <HeroBgPatternSectionStyle />
+          <Navbar />
+          <Hero />
+        </div>
+        <GetStarted />
+        <Features />
+        <Testimonials />
+        <div style={radialGradientStyle} className="relative">
+          <DowloadAdsBgPatternSectionStyle />
+          <DownloadAds />
+        </div>
+        <FAQ />
+        <CTA />
+        <Footer />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
